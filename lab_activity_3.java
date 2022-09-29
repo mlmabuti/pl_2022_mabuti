a6743fe6f9ae54f8c6b89a680046320d230dd143
Marjolo Mabuti <72655919+axojolotl@users.noreply.github.com>
2022-09-29 21:02:16 +0800

    update tokenizer
    Allow tokenizer to tokenize inputs with invalid syntax

--- lab_activity_3.java
+++ lab_activity_3.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class lab_activity_3 {
+public class lab_activity_3_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -10,46 +10,85 @@ public class lab_activity_3 {
         String input = sc.nextLine();
 
         System.out.print("Output is: ");
-        for (String str : tokenize(processInput(input))){
+        for (String str : tokenize(getLexemes(input))) {
             System.out.print(str + " ");
         }
     }
 
     public static ArrayList<String> tokenize(ArrayList<String> processedInput){
-        ArrayList<String> dataTypes = new ArrayList<>(Arrays.asList("int", "double", "char", "String"));
-        int values = 0, identifiers = 0;
-
-        ArrayList<String> tokens = new ArrayList<>();
+        ArrayList<String> dataTypes = new ArrayList<>(
+                Arrays.asList("int", "double", "char", "String")),
+                tokens = new ArrayList<>();
 
         for (String str : processedInput){
             if (dataTypes.contains(str)){
-            tokens.add("<data_type>");
+                tokens.add("<data_type>");
             } else if (str.contains("=")){
                 tokens.add("<assignment_operator>");
-            } else if (values == 0 && (str.contains("\"") || str.contains("'") || Character.isDigit(str.charAt(0))) ){
-                values++;
+            } else if (str.contains("\"")|| str.contains("'")
+                    || Character.isDigit(str.charAt(0))) {
                 tokens.add("<value>");
             } else if (str.contains(";")){
                 tokens.add("<delimiter>");
-            } else if (identifiers==0){
-                identifiers++;
+            } else {
                 tokens.add("<identifier>");
             }
         }
+
         return tokens;
     }
 
-    public static ArrayList<String> processInput(String input){
-        String[] inputSpecific = input.split(" ");
-        String lastWord = inputSpecific[inputSpecific.length-1],
-        lastCharacter = lastWord.split("")[lastWord.length()-1];
-        
-        lastWord = lastWord.substring(0, lastWord.length()-1);
-        ArrayList<String> inputSpecificFinal = new ArrayList<>(
-                Arrays.asList(inputSpecific).subList(0, inputSpecific.length - 1));
+    public static ArrayList<String> getLexemes(String input){
+        ArrayList<String> inputSpecific = new ArrayList<>(
+                Arrays.asList(input.split(" "))),
+                inputSpecificFinal, inputSpecificTemp;
 
-        inputSpecificFinal.add(lastWord);
-        inputSpecificFinal.add(lastCharacter);
+        inputSpecificTemp = extractSemiColon(inputSpecific);
+        inputSpecificFinal = mergeStringValues(inputSpecificTemp);
+
+        return inputSpecificFinal;
+    }
+
+    public static ArrayList<String> extractSemiColon(ArrayList<String> inputSpecific){
+        ArrayList<String> inputSpecificTemp = new ArrayList<>();
+
+        for (String str : inputSpecific){
+            if (str.contains(";")){
+                if (str.length() >= 2) {
+                    String word = str.substring(0,str.indexOf(";"));
+                    inputSpecificTemp.add(word);
+                }
+                inputSpecificTemp.add(";");
+            } else {
+                inputSpecificTemp.add(str);
+            }
+        }
+
+        return inputSpecificTemp;
+    }
+
+    public static ArrayList<String> mergeStringValues(ArrayList<String> inputSpecificTemp){
+        ArrayList<String> inputSpecificFinal = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        int quote = 0;
+
+        for (String str : inputSpecificTemp) {
+            if (str.charAt(0) == '"' && str.charAt(str.length()-1)=='"'){
+                inputSpecificFinal.add(str);
+            } else if (str.charAt(0) == '"') {
+                quote++;
+                temp.append(str).append(" ");
+            } else if (str.charAt(str.length() - 1) == '"') {
+                temp.append(str).append(" ");
+                inputSpecificFinal.add(temp.toString());
+                temp = new StringBuilder();
+                quote = 0;
+            } else if (quote == 1) {
+                temp.append(str).append(" ");
+            } else {
+                inputSpecificFinal.add(str);
+            }
+        }
 
         return inputSpecificFinal;
     }
